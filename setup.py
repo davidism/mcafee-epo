89c42220c262e015f1cb4fea040732822e4a4b82
David Lord <davidism@gmail.com>
2014-01-28 14:47:23 -0800

    package mcafee api

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='McAfee',
+    version='4.6.0',
+    url='https://community.mcafee.com/docs/DOC-3095',
+    license='',
+    author='McAfee, Inc.',
+    author_email='',
+    description='Sample python client for connecting to the ePO web APIs.',
+    py_modules=['mcafee', 'urlquote'],
+    zip_safe=True,
+)
