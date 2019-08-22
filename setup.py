66bf07faca17aa68cd9d37dcbeeab8fbe95a3466
David Lord <davidism@gmail.com>
2019-08-22 08:31:31 -0700

    switch to setup.cfg

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python
 from setuptools import setup
 
-setup(
-    name='mcafee-epo',
-    version='1.0.4',
-    url='https://bitbucket.org/davidism/mcafee-epo',
-    license='BSD',
-    author='David Lord',
-    author_email='david.lord@moesol.com',
-    description='McAfee ePolicy Orchestrator API',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: System :: Systems Administration'
-    ],
-    py_modules=['mcafee_epo'],
-    include_package_data=True,
-    zip_safe=True,
-    install_requires=['requests']
-)
+setup()
